aa1d71be6950cf860f9c98259b5b39c74c55279c
Ahmed <a7medmhhh@hotmail.com>
2018-03-18 14:51:40 +0200

    -add new book

---
+++ app/src/main/java/com/example/ahmed/librarysystem/Book.java
@@ -0,0 +1,51 @@
+package com.example.ahmed.librarysystem;
+
+
+
+public class Book {
+    private  int ISBN;
+    private  String name;
+    private  String publisher;
+    private  String pYear;
+    private  String author_name;
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getpYear() {
+        return pYear;
+    }
+
+    public void setpYear(String pYear) {
+        this.pYear = pYear;
+    }
+
+    public String getAuthor_name() {
+        return author_name;
+    }
+
+    public void setAuthor_name(String author_name) {
+        this.author_name = author_name;
+    }
+}
--- app/src/main/java/com/example/ahmed/librarysystem/MainActivity.java
+++ app/src/main/java/com/example/ahmed/librarysystem/MainActivity.java
@@ -4,20 +4,33 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 TextView user,email;
-ImageView image;
+EditText isbnE,nameBookE,pubE,yearE,authE;
 FirebaseUser firebaseUser;
+    Book book1 = new Book();
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference bookDB=firebaseDatabase.getReference("books");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,18 +39,40 @@ FirebaseUser firebaseUser;
         Button button = (Button) findViewById(R.id.button2);
         user = (TextView) findViewById(R.id.nameTxt);
         email = (TextView) findViewById(R.id.EmailTxt);
-        image = (ImageView) findViewById(R.id.imageView3);
+        isbnE = (EditText) findViewById(R.id.isbnTxt);
+        nameBookE = (EditText) findViewById(R.id.nameBookTxt);
+        pubE = (EditText) findViewById(R.id.pubTxt);
+        yearE = (EditText) findViewById(R.id.dateTxt);
+        authE = (EditText) findViewById(R.id.authTxt);
+
+
+
+
+
 
         if(firebaseUser!=null){
            user.setText( firebaseUser.getDisplayName());
            email.setText(firebaseUser.getEmail());
-           image.setImageURI(firebaseUser.getPhotoUrl());
+
         }
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
-                startActivity(intent);
+                book1.setName(nameBookE.getText().toString());
+                book1.setISBN(Integer.parseInt(isbnE.getText().toString()));
+                book1.setpYear(yearE.getText().toString());
+                book1.setAuthor_name(authE.getText().toString());
+                book1.setPublisher(pubE.getText().toString());
+                bookDB.push().setValue(book1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Post Add Successfully", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Error : post not add 🙁 " + task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
 
